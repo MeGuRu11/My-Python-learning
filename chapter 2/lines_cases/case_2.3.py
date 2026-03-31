39d21a8bfe2682759dfc68959744400d8a9d4915
MeGuRu11 <krawchencko@mail.ru>
2026-03-31 12:04:55 +0300

    lines

---
+++ chapter 2/lines_cases/case_2.3.py
@@ -0,0 +1,3 @@
+user = "MeGuRu_11"
+message = f"Здравствуйте, {user}, не хотите ли вы изучить Python сегодня?"
+print(message)
---
+++ chapter 2/lines_cases/case_2.4.py
@@ -0,0 +1,4 @@
+name = "крУтОй ГОНщик"
+print(name.upper())
+print(name.lower())
+print(name.title())
